b8c0e175386bb33db1eb24645d0d220b479fc489
Nano <hyeonpass@naver.com>
2025-03-08 02:05:22 +0900

    Update handleHeadings.php

--- parser/handleHeadings.php
+++ parser/handleHeadings.php
@@ -1,10 +1,10 @@
 <?php
 function handleHeadings( $text ) {
-		for ( $i = 6; $i >= 1; --$i ) {
-			$h = str_repeat( '=', $i );
-			// Trim non-newline whitespace from headings
-			$text = preg_replace( "/^(?:$h)[ \\t]*(.+?)[ \\t]*(?:$h)\\s*$/m", "<h$i>\\1</h$i>", $text );
-		}
-		return $text;
-	}
+  for ( $i = 6; $i >= 1; --$i ) {
+    $h = str_repeat( '=', $i );
+    // Trim non-newline whitespace from headings
+    $text = preg_replace( "/^(?:$h)[ \\t]*(.+?)[ \\t]*(?:$h)\\s*$/m", "<h$i>\\1</h$i>", $text );
+  }
+    return $text;
+}
 ?>
